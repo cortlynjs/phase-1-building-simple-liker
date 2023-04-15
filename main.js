7590ec2c9c72e713971ba1639c2ce0df392da6f0
Cortlyn Stafford <cortlynstafford@icloud.com>
2023-04-15 16:22:31 -0400

    simple liker app

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Hide the error modal on page load
+const modal = document.querySelector('#modal');
+modal.classList.add('hidden');
+
+function likeCallback(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      // Toggle the heart icon
+      if (heart.classList.contains('activated-heart')) {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      } else {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      }
+    })
+    .catch((error) => {
+      // Show the error modal with the error message
+      modal.classList.remove('hidden');
+      const modalMessage = document.querySelector('#modal-message');
+      modalMessage.textContent = error;
+      // Hide the error modal after 3 seconds
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+const hearts = document.querySelectorAll('.like-glyph');
+for (const heart of hearts) {
+  heart.addEventListener('click', likeCallback);
+}
 
 
 
